4978822dad81ff42fc9ff1dfa886b8af97af6bfe
Aloba Taiwo <98100387+Taiwo052@users.noreply.github.com>
2023-04-04 17:15:57 +0100

    Added basketball scoreboard files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+let homeScoreEl = document.getElementById("home_score")
+let guestScoreEl = document.getElementById("guest_score")
+let homeScoreBtn1 = document.getElementById("home_score_btn1")
+let homeScore = 0
+let guestScore = 0
+
+console.log(homeScore)
+
+function start(){
+    homeScoreEl.textContent = 0
+    guestScoreEl.textContent = 0
+} 
+
+function homeIncrementOne(){
+    homeScore += 1
+    homeScoreEl.textContent = homeScore
+} 
+
+function homeIncrementTwo(){
+    homeScore += 2
+    homeScoreEl.textContent = homeScore
+} 
+
+function homeIncrementThree(){
+    homeScore += 3
+    homeScoreEl.textContent = homeScore
+} 
+
+function guestIncrementOne(){
+    guestScore += 1
+    guestScoreEl.textContent = guestScore
+} 
+
+function guestIncrementTwo(){
+    guestScore += 2
+    guestScoreEl.textContent = guestScore
+} 
+
+function guestIncrementThree(){
+    guestScore += 3
+    guestScoreEl.textContent = guestScore
+} 
